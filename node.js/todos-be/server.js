5f1f86c29c6201c65af6f7705f23a28eb35e210b
kiwi lin <cyc40405@gmail.com>
2021-10-30 14:36:59 +0800

    bluebird json輸入

--- node.js/todos-be/server.js
+++ node.js/todos-be/server.js
@@ -1,20 +1,52 @@
 const express = require("express");
+const app = express();
+const mysql = require("mysql");
+const Promise = require("bluebird");
+require("dotenv").config();
 
-// 路由
-// app.Method(path,Handler)
-// Method:GET,POST,PUT,DELETE,PATCH,....
+// connect ro DB
+var connection = mysql.createConnection({
+  host: process.env.DB_HOST,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_NAME,
+});
+connection = Promise.promisifyAll(connection);
+connection.connect();
 
-let app = express();
+// middleware
+app.use((req, res, next) => {
+  console.log("Hit this middleware!!!");
+  next();
+});
 
 app.get("/", (req, res) => {
-  res.send("我是 Express 首頁");
+  res.send("Homepage");
 });
 
-app.get("/member", (req, res) => {
-  res.send("我是 member");
+app.get("/api/member", (req, res) => {
+  connection.query("SELECT * FROM members", function (error, results, fields) {
+    if (error) throw error;
+    console.log(results);
+    res.json(results);
+  });
+  connection.end();
 });
 
-// 3001 port
-app.listen(3001, () => {
-  console.log("express app 啟動了噢");
+app.get("/post", (req, res) => {
+  res.send("post page");
+});
+
+app.get("/api/todos", async (req, res) => {
+  let data = await connection.queryAsync("SELECT * FROM todos");
+  res.json(data);
+  console.log(data);
+});
+
+app.use((req, res, next) => {
+  res.send("404 page not found!!!");
+});
+
+app.listen(3502, () => {
+  console.log("running on port 3502");
 });
